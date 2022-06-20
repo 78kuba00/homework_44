2a63648dd56afef785130dc268bdfbd3410489fd
aidana <78kuba00@gmail.com>
2022-06-20 22:49:06 +0600

    ispravlennyi propotip igry

--- source/webapp/views.py
+++ source/webapp/views.py
@@ -5,28 +5,32 @@ from .game import Game
 def play(request):
     secret_number = [1, 2, 3, 4]
     if request.method == "GET":
-        return render(request, "index.html")
+        context = {
+            "result": None,
+            "message": "",
+        }
+        return render(request, "index.html", context)
     else:
         numbers_str = str(request.POST.get('numbers')).split()
         if not Game.is_valid_count(numbers_str):
             context = {
-                "success": False,
+                "result": 'error',
                 "message": "Please enter 4 number spearated by space",
             }
         elif not Game.is_unique_numbers(numbers_str):
             context = {
-                "success": False,
+                "result": 'error',
                 "message": "Numbers are not unique",
             }
         elif not Game.is_valid_range(numbers_str):
             context = {
-                "success": False,
+                "result": 'error',
                 "message": "Numbers must be 1-9",
             }
         else:
             result = Game.play(numbers_str, secret_number)
             context = {
-                "success": True,
+                "result": 'success',
                 "message": f"Cows {len(result[0])} Bulls {len(result[1])}",
             }
 
